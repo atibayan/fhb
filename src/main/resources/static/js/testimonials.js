b39603be973569724d3cd42a061f462f2a669d9c
cristi <cristilive6@gmail.com>
2023-07-01 13:16:23 -0700

    linking css and working on setting up fake user testimonials

---
+++ src/main/resources/static/js/testimonials.js
@@ -0,0 +1,30 @@
+// alert("testimonials.js was triggered correctly ")
+
+
+
+
+
+class App extends React.Component{
+    state={
+        all_testimonials:[
+            {
+
+            },
+        ]
+    }
+
+
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
